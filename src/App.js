0d3cd55c0b62af902bfafbe75ceef04ab926add9
Digambaran <arjunsreekumar.1@gmail.com>
2022-12-28 13:00:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sample_otp_verification_page from './sample_otp_verification_page'
+export default function App() {
+  return (
+    <div>
+      <Sample_otp_verification_page/>
+    </div>
+  )
+}
---
+++ src/sample_otp_verification_page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Sample_otp_verification_page = () => {
+  return (
+   <p>Hello from sample_otp_verification_page </p>
+  )
+}
+export default Sample_otp_verification_page
